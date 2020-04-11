7b81d130c6737c7a9914d5693a484a2426ce9d74
BogdanovCoding <BogdanovCoding@gmail.com>
2020-04-11 15:29:13 -0500

    bogdanov_personal-project-01-heroku

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/admin/Desktop/BogdanovCoding/3_code_platoon_code/personal_project/personal_project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
